e87ec62564e28320ea64ef10aae9aec8af3f01c3
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-06-29 13:01:34 -0300

    Outputting dynamic content by using JS in the returned JSX

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ class App extends Component {
         <Person />
         <Person />
         <Person />
-        <Person /> 
       </div>
     );
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react' 
 
 const Person = () => {
-  return <p>I'm a person!</p>
+  return <p>I'm a person and I'm { Math.floor(Math.random() * 30) } years old</p>
 }
 
 export default Person
