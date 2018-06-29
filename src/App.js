7f4153fc6d165dfff81e155c6253c13a8a819b36
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-06-29 12:38:22 -0300

    Understanding JSX by using React.createElement() and styling it with className

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
+    /* return (
       <div className="App">
         <h1>Hello</h1>
       </div>
-    );
+    ); */
+
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Understanding JSX!'))
   }
 }
 
