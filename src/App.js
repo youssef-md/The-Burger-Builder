b91ca1af0ff9731a0b346015de3ff228eb43503f
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-06-30 11:07:52 -0300

    Working with class state

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import './App.css';
 import Person from './Person/Person' //firs letter must be capital so React identifies as custom component
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Youssef', age: 18 },
+      { name: 'Alice', age: 19},
+      { name: 'Joseliton', age: 20}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello</h1>
-        <Person name="Youssef" age="18" >My hobbies: Programming</Person>
-        <Person name="Alice" age="19" />
+        <Person name = { this.state.persons[0].name } age = { this.state.persons[0].age } >My hobbies: Programming</Person>
+        <Person name = { this.state.persons[1].name } age = { this.state.persons[0].age } />
+        <Person name = { this.state.persons[2].name } age = { this.state.persons[0].age } />
       </div>
     );
 
