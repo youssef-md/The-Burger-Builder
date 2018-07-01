1e42cd5705d36c3263f7bd40dd9eae7681cb966c
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-01 10:44:37 -0300

    Adding styling with Stylesheets

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react' 
+import './Person.css'
 
 const Person = (props) => {
   return (
-    <div>
+    <div className = "Person">
       <p>I'm { props.name } and I'm { props.age } years old</p>
       <p>{ props.children }</p>
       <button onClick = { props.click }>switch names from presentational component</button>
