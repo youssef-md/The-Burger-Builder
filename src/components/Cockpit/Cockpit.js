509d9994e3dcc7e4bcf1554e1dea8261885db5c8
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-05 20:49:13 -0300

    Refactoring the initial message with Cockpit component

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from './Cockpit.css'
+
+const Cockpit = (props) => {
+
+  const classes = []
+  let btnClass = ''
+
+  if(props.showPersons)
+    btnClass = styles.Red
+
+  if(props.persons.length <= 2) 
+    classes.push(styles.red)
+
+  if(props.persons.length <= 1)
+    classes.push(styles.bold)
+
+  return (
+    <div className = { styles.Cockpit }>
+      <h1>Hello</h1>        
+      <p className = { classes.join(' ') }>This is really working</p>
+      <button  
+        className = { btnClass }
+        onClick = { props.clicked }>
+          Toggle Persons
+      </button>
+    </div>
+  )
+}
+
+export default Cockpit
