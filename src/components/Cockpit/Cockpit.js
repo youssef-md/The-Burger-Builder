507b1c31a7e591eace6cac770be0209b46c31eb8
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-07 22:48:38 -0300

    Using a HOC that just return children for not creating a new div and messing the styling

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import styles from './Cockpit.css'
+import Aux from '../hoc/Aux'
 
 const Cockpit = (props) => {
 
   const classes = []
-  let btnClass = ''
+  let btnClass = styles.Button
 
   if(props.showPersons && props.persons.length !== 0)
-    btnClass = styles.Red
+    btnClass = [styles.Button, styles.Red].join(' ') //Button Red
 
   if(props.persons.length <= 2) 
     classes.push(styles.red)
@@ -16,16 +17,16 @@ const Cockpit = (props) => {
     classes.push(styles.bold)
 
   return (
-    <div className = { styles.Cockpit }>
+    <Aux>
       <h1>{ props.appTitle }</h1>        
       <p className = { classes.join(' ') }>This is really working</p>
       <button  
-        className = { btnClass }
-        onClick = { props.clicked }>
-          Toggle Persons
-      </button>
-    </div>
-  )
+      className = { btnClass }
+      onClick = { props.clicked }>
+            Toggle Persons
+        </button>
+    </Aux>
+)
 }
 
 export default Cockpit
