0e57b263d44f46443402d0ef8e3c6525c232f7f0
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-08 23:06:25 -0300

    Validating props by using PropTypes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react' 
 import styles from './Person.css'
 import withClass from '../../hoc/withClass'
+import PropTypes from 'prop-types'
 
 class Person extends Component {
 
@@ -29,5 +30,11 @@ class Person extends Component {
   }
 }
 
+Person.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  click: PropTypes.func,
+  changed: PropTypes.func
+}
 
 export default withClass(Person, styles.Person)
