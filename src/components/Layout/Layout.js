2c8f05438928feefc219d40c46dbe8defae44d32
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-10 19:11:08 -0300

    BurguerBuilder container and wrapping it with the Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Aux from '../../hoc/Aux'
+import styles from './Layout.css'
 
 const Layout = (props) => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className = {styles.content}>
       {props.children}
     </main>
 
