1cd159cf6476777d3be7f55208e548a271fe6ad4
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-09 22:50:48 -0300

    Using the ref for focusing on the input of the first person of the list

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -16,6 +16,8 @@ class Person extends Component {
 
   componentDidMount() {
     console.log('[Person.js] Inside DidMount()')
+    if(this.props.position === 0)
+      this.inputElement.focus()
   }
   render() {
     console.log('[Person.js] Inside render()')
@@ -24,7 +26,11 @@ class Person extends Component {
         <p>I'm { this.props.name } and I'm { this.props.age } years old</p>
         <p>{ this.props.children }</p>
         <button className = {styles.deleteBtn} onClick = { this.props.click }>DELETE</button>
-        <input type = "text" onChange = { this.props.changed } value = { this.props.name }/>
+        <input 
+          type = "text" 
+          onChange = { this.props.changed } 
+          value = { this.props.name }
+          ref = { (input) => { this.inputElement = input }} />
       </Fragment>
     )
   }
