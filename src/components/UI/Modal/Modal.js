7de5d6b4ecde8de31cbf71ff3f087e933d73b635
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-13 20:55:44 -0300

    The Modal component will only update if there is a change in props.show

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,25 +2,34 @@ import React, { Fragment } from 'react';
 import styles from './Modal.css'
 import Backdrop from '../Backdrop/Backdrop'
 
-const Modal = (props) => {
+class Modal extends React.Component {
 
-  const inlineStyle = {
-    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-    opacity: props.show ? '1' : '0'
+  shouldComponentUpdate(nextProps, nextState) {
+    //update only if the user clicked in the Order button
+    return this.props.show !== nextProps.show
   }
-  
-  return (
 
-  <Fragment>
-    <Backdrop show = { props.show } click = {props.modalClose}/>
-    <div 
-      className = { styles.Modal }
-      style = { inlineStyle }> 
-          { props.children } 
-    </div>
-  </Fragment>
-  )
+  componentDidUpdate() {
+    console.log('Modal updated')
+  }
 
+  render() {
+    const inlineStyle = {
+      transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+      opacity: this.props.show ? '1' : '0'
+    }
+
+    return (
+      <Fragment>
+        <Backdrop show = { this.props.show } click = {this.props.modalClose}/>
+        <div 
+          className = { styles.Modal }
+          style = { inlineStyle }> 
+              { this.props.children } 
+        </div>
+      </Fragment>
+    )
+  }
 }
 
 
