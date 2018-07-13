b2a2e7d16dd3c6e25070223c3a7db76395d908bc
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-13 17:06:42 -0300

    Styling the Sidedrawer button

--- src/components/Navigation/Toolbar/MenuButton/MenuButton.js
+++ src/components/Navigation/Toolbar/MenuButton/MenuButton.js
@@ -2,7 +2,10 @@ import React from 'react'
 import styles from './MenuButton.css'
 
 const MenuButton = (props) => (
-  <div  onClick = { props.click }>Menu
+  <div className = { styles.MenuButton } onClick = { props.click }>
+    <div></div>
+    <div></div>
+    <div></div>
   </div>
 );
 
