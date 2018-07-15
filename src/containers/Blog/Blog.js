08f948f148a4289709f555f02776cd1b06d50373
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-14 22:48:00 -0300

    Using Axios for doing a request based in promises of JSONPlaceholder and displaying the response

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios'
 
 class Blog extends Component {
+
+    componentDidMount() {
+        axios.get('http://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                console.log(response)
+            })
+    }
+
     render () {
         return (
             <div>
