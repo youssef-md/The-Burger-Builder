7e44a9b19a7b749a64880c54179e5206664331ba
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-15 23:53:03 -0300

    Ajax with axios for getting the response with the clicked ID Post

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import './FullPost.css';
 
 class FullPost extends Component {
+
+    componentDidUpdate() {
+        if(this.props.id !== null)
+            axios.get(`http://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+            .then(response => {
+                console.log(response)
+            })
+    }
+
     render () {
         let post = <p style = {{textAlign: 'center'}}>Please select a Post!</p>
         
