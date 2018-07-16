41750ab3e813678f8aa0aa1b7ae487166621f709
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-16 00:34:07 -0300

    Fetching data on Update without update request loop(setState inside didUpdate)

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -5,22 +5,33 @@ import './FullPost.css';
 
 class FullPost extends Component {
 
+    state = {
+        loadedPost: null
+    }
+
     componentDidUpdate() {
-        if(this.props.id !== null)
-            axios.get(`http://jsonplaceholder.typicode.com/posts/${this.props.id}`)
-            .then(response => {
-                console.log(response)
-            })
+        if(this.props.id !== null) {
+            if( !this.state.loadedPost || (this.state.loadedPost !== null && this.state.loadedPost.id !== this.props.id)) {
+
+                axios.get(`http://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+                .then(response => {
+                    this.setState({loadedPost: response.data})
+                })
+            }
+        }        
     }
 
     render () {
         let post = <p style = {{textAlign: 'center'}}>Please select a Post!</p>
         
-        if(this.props.id !== null){
+        if(this.props.id !== null)
+            post = <p style = {{textAlign: 'center'}}>Loading...</p>
+
+        if(this.state.loadedPost !== null){
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{ this.state.loadedPost.title }</h1>
+                    <p>{ this.state.loadedPost.body }</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
