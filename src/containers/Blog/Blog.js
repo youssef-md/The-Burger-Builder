81ae0fb895f7bcc559035d0b4d785e099291c72f
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-16 19:43:13 -0300

    Handling errors locally

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ class Blog extends Component {
 
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount() {
@@ -19,21 +20,28 @@ class Blog extends Component {
                 const updatedPosts = posts.map(post => {
                     return { ...post, author: 'Youssef'}
                 })
-
                 this.setState({posts: updatedPosts})
                 console.log(this.state.posts)
             })
+            .catch(error => {
+                console.log(error)
+                this.setState({error: true})
+            });
     }
 
     render () {
 
-        const posts = this.state.posts.map(post => {
-            return <Post 
-                        key = { post.id } 
-                        title = { post.title } 
-                        author = { post.author }
-                        clicked = { () => this.selectedPostHandler(post.id) }/>
-        })
+        let posts = <p style = {{textAlign: 'center', fontSize: '2em'}}>Something went wrong!</p>
+
+        if(!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post 
+                            key = { post.id } 
+                            title = { post.title } 
+                            author = { post.author }
+                            clicked = { () => this.selectedPostHandler(post.id) }/>
+            })
+        }
 
         return (
             <div>
