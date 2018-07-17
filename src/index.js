5312cab79d2378b8a81a4e2db92edb1da374cb5e
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-16 23:27:42 -0300

    Creating and using Axios instances for a specific configuration

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com'
-axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN'
+//axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN'
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 
 axios.interceptors.request.use(requestConfig => {
