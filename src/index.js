1bd72362108149b914bbc71649016ec9558acf8f
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-16 23:13:26 -0300

    Adding Interceptors to execute code globally: no internet connection

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios'
+
+axios.interceptors.request.use(requestConfig => {
+  console.log(requestConfig)
+  return requestConfig
+}, error => { // error for no internet connection
+  console.log(error)
+  return Promise.reject(error)
+})
+
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
