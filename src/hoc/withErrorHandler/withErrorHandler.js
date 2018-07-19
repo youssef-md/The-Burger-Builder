230b8a1051614c817fe3a76464ff585b71823bd5
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-19 00:04:37 -0300

    returning an anonymous class that handle request/response errors using axios

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,16 +1,40 @@
 import React, { Fragment } from 'react'
 import Modal from '../../components/UI/Modal/Modal'
 
-const withErrorHandler = (WrappedComponent) => {
-  return (props) => { //returning a function
-    return (
-      <Fragment>
-        <Modal>
-          Something didn't work
-        </Modal>
-        <WrappedComponent {...props} />
-      </Fragment>
-    )
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends React.Component { //returning a class
+    
+    state = {
+      error: null
+    }
+
+    componentDidMount() {
+      axios.interceptors.request.use(request => {
+        this.setState({error: null})
+        return request
+      })
+
+      axios.interceptors.response.use(response => response, error => {
+        this.setState({error: true})
+      })
+    }
+    
+    render() {
+
+      return (
+        <Fragment>
+          <Modal show = {this.state.error} modalClose = {this.dismissError}>
+            Something didn't work
+            {this.state.error ? this.state.error : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Fragment>
+      )
+    }
+
+    dismissError = () => {
+      this.setState({error: null})
+    }
   }
 }
 
