bab72518d402cfb4464f193d0e1ad7d20b3de648
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-20 23:51:51 -0300

    Using Link component instead of <a> for switching pages and maintaing the state by not doing a new request

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import './Blog.css';
 import axios from '../../axios'
 import Posts from './Posts/Posts'
 import NewPost from './NewPost/NewPost'
-import { Route } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 
 
 class Blog extends Component {
@@ -15,8 +15,8 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/new-post">New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
