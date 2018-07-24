ee83cf52edf22b86791e478048c7a78ef7cb5215
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-23 22:14:47 -0300

    Loading component async, by using Lazy Loading, to make sure that the user downloads only the needed page

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import './Blog.css';
 import axios from '../../axios'
 import Posts from './Posts/Posts'
-import NewPost from './NewPost/NewPost'
+//import NewPost from './NewPost/NewPost'
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
+import asyncComponent from '../../hoc/asyncComponent'
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost') // Will only import NewPost when the const is used
+})
 
 class Blog extends Component {
 
     state = {
-        auth: false
+        auth: true
     }
 
     render () {
@@ -24,7 +29,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    { this.state.auth ? <Route path = "/new-post" component = { NewPost }/> : null }
+                    { this.state.auth ? <Route path = "/new-post" component = { AsyncNewPost }/> : null }
                     <Route path = "/posts/" component = { Posts } />
                     <Route render = {() => <h1>404 Not Found</h1>} />
                     {/* <Redirect from = "/" to = "/posts/" /> */}
