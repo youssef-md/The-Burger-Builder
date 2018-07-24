74574119cddfaca3e080e1f2c7858e1cd6036b82
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-23 22:29:54 -0300

    Test project

--- src/components/UI/Spinner/Spinner.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './Spinner.css'
-
-const Spinner = () => (
-  <div className={styles.lds_ellipsis}><div></div><div></div><div></div><div></div></div>
-)
-
-export default Spinner
