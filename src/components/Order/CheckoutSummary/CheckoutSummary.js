53c613493fde75b8fb3794cac86df3a8dfbb108b
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-24 23:39:49 -0300

    Building the Checkout container

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import Burger from '../../Burger/Burger'
 import Button from '../../UI/Button/Button'
-import styles from './CheckoutSummary'
+import styles from './CheckoutSummary.css'
 
 const CheckoutSummary = (props) => {
 
   return(
     <div className = {styles.CheckoutSummary}>
       <h1>We hope it tastes well!</h1>
-      <div style = {{width: '300px', height: '300px', margin: 'auto'}}>
+      <div style = {{width: '100%',  margin: 'auto'}}>
         <Burger ingredients = {props.ingredients}/>
       </div>
       
-      <Button btnType = 'Danger' click >
-        CANCEL
-      </Button>
-      
-      <Button btnType = 'Success' click >
-        CONTINUE
-      </Button>
+      <div className = {styles.Buttons}>
+        <Button btnType = 'Danger' click >
+          CANCEL
+        </Button>
+        
+        <Button btnType = 'Success' click >
+          CONTINUE
+        </Button>
+      </div>
 
     </div>
   )
