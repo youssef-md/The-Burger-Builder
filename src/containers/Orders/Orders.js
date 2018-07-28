a5ba329804920dcf579500e49e71f193b953f58b
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-27 23:59:27 -0300

    Getting the orders from firebase and updating the state

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import Order from '../../components/Order/Order'
+import axios from '../../axios-orders'
 
 class Orders extends React.Component {
+
+  state = {
+    orders: [],
+    loading: true,
+  }
+
+  componentDidMount() {
+    axios.get('orders.json').then(response => {
+      
+      const fetchedOrders = []
+      for(let key in response.data) {
+        fetchedOrders.push({
+          ...response.data[key],
+          id: key
+        })
+      }
+      console.log(fetchedOrders)
+      this.setState({ loading: false, orders: fetchedOrders })
+    
+    }).catch(err => {
+      this.setState({ loading: false })
+    })
+  }
 
   render() {
     return(
