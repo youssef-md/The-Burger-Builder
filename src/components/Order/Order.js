4697899a2b41bdb21e69c9fe892a2354e27a5e81
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-27 23:35:03 -0300

    Orders page, and setting up the Route to it

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './Order.css'
 
 const order = (props) => (
-  <div>
+  <div className = { styles.Order }>
     <p>Ingredients: Salad (1)</p>
     <p>Price: <strong>$ 2.00</strong></p>
   </div>
