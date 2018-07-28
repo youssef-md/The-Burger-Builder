209c6feba25740057fc9c77736903ed2e0304fb5
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-27 23:51:34 -0300

    Refactoring the Spinner

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './Spinner.css'
 
 const Spinner = () => (
-  <div className={styles.lds_ellipsis}><div></div><div></div><div></div><div></div></div>
+  <div className={ styles.lds_facebook }><div></div><div></div><div></div></div>
 )
 
 export default Spinner
