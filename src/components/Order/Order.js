edf3a2e294e180850d42ae4b7d30585352041b9e
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-07-27 23:30:00 -0300

    Creating the structure to work on the Orders page

---
+++ src/components/Order/Order.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Order.css'
+
+const order = (props) => (
+  <div>
+    <p>Ingredients: Salad (1)</p>
+    <p>Price: <strong>$ 2.00</strong></p>
+  </div>
+)
+
+export default Order
