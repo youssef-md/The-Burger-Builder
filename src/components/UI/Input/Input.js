d4f693309edfe0f1b0143cdbe2a55c61356bccda
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-08-01 00:34:30 -0300

    Handling dropdown into the input component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -12,6 +12,15 @@ const Input = (props) => {
     case ('textarea'):
       inputElement = <textarea className={ styles.InputElement } {...props.elementConfig} value={ props.value }/>
       break
+    case ('select'):
+      inputElement = <select className={ styles.InputElement } value={props.value}>
+                      {props.elementConfig.options.map(option => (
+                        <option key = { option.value } value = { option.value }>
+                          { option.displayValue }
+                        </option>
+                      ))}
+                     </select>
+      break
     default:
       inputElement = <input className={ styles.InputElement } {...props.elementConfig} value={ props.value }/>
       break
