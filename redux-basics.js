99766439008ab352e789e07f14d24726449aa5d9
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-08-05 22:22:57 -0300

    Dispatching actions

--- redux-basics.js
+++ redux-basics.js
@@ -7,6 +7,14 @@ const initialState = {
 
 //Reducer
 const rootReducer = (currentState = initialState, action) => {
+  if(action.type === 'INC_COUNTER') {
+    return { ...currentState, counter: currentState.counter + 1 } // immutable fashion
+  }
+
+  if(action.type === 'ADD_COUNTER') {
+    return { ...currentState, counter: currentState.counter + action.value}
+  }
+
   return currentState
 }
 
@@ -15,6 +23,8 @@ const store = createStore(rootReducer)
 console.log(store.getState())
 
 //Dispatching Action
-
+store.dispatch({ type: 'INC_COUNTER' })
+store.dispatch({ type: 'ADD_COUNTER', value: 10 })
+console.log(store.getState())
 
 //Subscription
