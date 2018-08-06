9853d66dec3aca99fd95519744a09eef2803be37
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-08-06 20:05:00 -0300

    Passing and retrieving data with action

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -31,8 +31,8 @@ class Counter extends Component {
 							<CounterOutput value={this.props.counter} />
 							<CounterControl label="Increment" clicked={ this.props.onIncrementCounter } />
 							<CounterControl label="Decrement" clicked={ this.props.onDecrementCounter }  />
-							<CounterControl label="Add 5" clicked={ this.props.onAddCounter }  />
-							<CounterControl label="Subtract 5" clicked={ this.props.onSubtract }  />
+							<CounterControl label="Add 10" clicked={ this.props.onAddCounter }  />
+							<CounterControl label="Subtract 10" clicked={ this.props.onSubtract }  />
 					</div>
 			);
 	}
@@ -48,8 +48,8 @@ const mapDispatchToProps = dispatch => {
 	return {
 		onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
 		onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
-		onAddCounter: () => dispatch({ type: 'ADD', value: 5 }),
-		onSubtract: () => dispatch({ type: 'SUBTRACT', value: 5})
+		onAddCounter: () => dispatch({ type: 'ADD', value: 10 }),
+		onSubtract: () => dispatch({ type: 'SUBTRACT', value: 10 })
 	}
 }
 
