cbd32c1b8f2358790d9a5fc97bcb9a921289b77d
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-08-07 23:58:19 -0300

    Returning an id containing the snapshot of the date to use it as an ID for mapping the <li>

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -29,7 +29,7 @@ const reducer = (state = initialState, action) => {
     case 'STORE_RESULT':
       return {
         ...state,
-        results: state.results.concat(state.counter)
+        results: state.results.concat({id: new Date(), value: state.counter})
       }
     default:
       return state
