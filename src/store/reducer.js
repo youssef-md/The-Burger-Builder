e9c69fa07b3723caf8fbb5723045d40c36a87a93
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-08-08 23:47:27 -0300

    Using the outsorted actiong in the reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,5 @@
+import * as actionType from './actions'
+
 const initialState = {
   counter: 0,
   results: []
@@ -6,32 +8,32 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   
   switch (action.type) {
-    case 'INCREMENT':
+    case actionType.INCREMENT:
       return {
         ...state,
         counter: state.counter + 1
       }
-    case 'DECREMENT':
+    case actionType.DECREMENT:
       return {
         ...state,
         counter: state.counter - 1 
       }
-    case 'ADD':
+    case actionType.ADD:
       return {
         ...state,
         counter: state.counter + action.value
       }
-    case 'SUBTRACT':
+    case actionType.SUBTRACT:
       return {
         ...state,
         counter: state.counter - action.value
       }
-    case 'STORE_RESULT':
+    case actionType.STORE_RESULT:
       return {
         ...state,
         results: state.results.concat({ id: new Date(), value: state.counter })
       }
-    case 'DELETE_RESULT':
+    case actionType.DELETE_RESULT:
       const updatedResults = state.results.filter(el => el.id !== action.elementID)
       return {
        ...state,
