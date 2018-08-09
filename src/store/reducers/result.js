cedb97932bc930ff1219ff8ddcc24803c55751b5
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-08-08 23:54:27 -0300

    Separating the reducers by their responsabilities

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,33 +1,12 @@
-import * as actionType from './actions'
+import * as actionType from '../actions'
 
 const initialState = {
-  counter: 0,
   results: []
 }
 
 const reducer = (state = initialState, action) => {
   
   switch (action.type) {
-    case actionType.INCREMENT:
-      return {
-        ...state,
-        counter: state.counter + 1
-      }
-    case actionType.DECREMENT:
-      return {
-        ...state,
-        counter: state.counter - 1 
-      }
-    case actionType.ADD:
-      return {
-        ...state,
-        counter: state.counter + action.value
-      }
-    case actionType.SUBTRACT:
-      return {
-        ...state,
-        counter: state.counter - action.value
-      }
     case actionType.STORE_RESULT:
       return {
         ...state,
