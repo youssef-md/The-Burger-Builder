19fb1608e4c7f8faef68de689805605f00110d61
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-08-13 21:03:07 -0300

    Connecting ContactData component to the Store

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import styles from './ContactData.css'
 import axios from '../../../axios-orders'
 import Spinner from '../../../components/UI/Spinner/Spinner'
 import Input from '../../../components/UI/Input/Input'
+import { connect } from 'react-redux'
 
 class ContactData extends React.Component {
 
@@ -203,5 +204,11 @@ class ContactData extends React.Component {
   }
 }
 
-export default ContactData
+const mapStateToProps = state => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice
+  }
+}
+export default connect(mapStateToProps)(ContactData)
 
