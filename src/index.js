d4434a9c2bbb80cc08f7dc793fbd44f2d43b32bf
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-08-15 00:01:38 -0300

    Applying the middleware and logging the change in the state

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 
 import counterReducer from './store/reducers/counter'
@@ -25,7 +25,7 @@ const logger = store => {
   }
 }
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 ReactDOM.render(<Provider store = { store }><App/></Provider>, document.getElementById('root'));
 registerServiceWorker();
