4ac7a950b20449fb35f40e8b25a479472eb0e9ba
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-09-09 23:52:58 -0300

    structure for the order's reducer

---
+++ src/store/reducers/order.js
@@ -0,0 +1,24 @@
+import * as actionTypes from '../actions/actionTypes'
+
+const initialState = {
+  orders: [],
+  loading: false  
+}
+
+const reducer = (state = initialState, action) => {
+
+  switch (action.type) {
+    case actionTypes.PURCHASE_BURGER_SUCCESS:
+      return {
+
+      }
+    case actionTypes.PURCHASE_BURGER_FAIL:
+      return {
+
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer
